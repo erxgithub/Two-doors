36b24ba0ec6f40960730d64af55a7686889ddbfa
erxgithub <erxlink@gmail.com>
2017-11-18 17:19:49 -0500

    Initial Commit

---
+++ Two doors/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Two doors
+//
+//  Created by Eric Gregor on 2017-11-18.
+//  Copyright © 2017 Eric Gregor. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
